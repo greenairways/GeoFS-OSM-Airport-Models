fa97e03d3a89a02019b23d81c834b07694989e4d
greenairways <green64502429@gmail.com>
2026-06-21 11:57:33 -0400

    Update defaultbuildingremover.js

--- defaultbuildingremover.js
+++ defaultbuildingremover.js
@@ -23,7 +23,16 @@
         "https://data.geo-fs.com/buildings/10_1673_384.glb",
         "https://data.geo-fs.com/buildings/10_1672_384.glb",
         "https://data.geo-fs.com/buildings/10_1674_385.glb",
-        "https://data.geo-fs.com/buildings/10_1673_386.glb"
+        "https://data.geo-fs.com/buildings/10_1673_386.glb",
+        "https://data.geo-fs.com/buildings/10_1674_384.glb",
+        "https://data.geo-fs.com/buildings/11_3346_769.glb",
+        "https://data.geo-fs.com/buildings/11_3347_769.glb",
+        "https://data.geo-fs.com/buildings/11_3348_769.glb",
+        "https://data.geo-fs.com/buildings/11_3346_770.glb",
+        "https://data.geo-fs.com/buildings/11_3347_770.glb",
+        "https://data.geo-fs.com/buildings/11_3348_770.glb",
+        "https://data.geo-fs.com/buildings/11_3346_771.glb",
+        "https://data.geo-fs.com/buildings/11_3347_771.glb"
     ];
 
     // Helper function to check if a URL should be blocked
