b884ebaecf7257892247e1331910e0524d3b634c
greenairways <green64502429@gmail.com>
2026-06-21 02:59:58 -0400

    Update defaultbuildingremover.js

--- defaultbuildingremover.js
+++ defaultbuildingremover.js
@@ -18,7 +18,8 @@
         "https://data.geo-fs.com/buildings/10_1672_385.glb",
         "https://example-another-asset.com/file.glb",
         "https://data.geo-fs.com/some-other-model.glb",
-        "https://data.geo-fs.com/buildings/10_1673_385.glb"
+        "https://data.geo-fs.com/buildings/10_1673_385.glb",
+        "https://data.geo-fs.com/buildings/10_1672_384.glb"
     ];
 
     // Helper function to check if a URL should be blocked
